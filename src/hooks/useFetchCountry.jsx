33f54de733e7e8b2151e45e7c88d4eaf4dfa7a50
Dima Savchuk <118117711+DimaSavchuk@users.noreply.github.com>
2023-06-24 15:14:41 +0300

    commit

---
+++ src/hooks/useFetchCountry.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/country-service';
+
+export const useFetchCountry = () => {
+  const [country, setCountry] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const { id } = useParams();
+  console.log(id);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+
+        const fetchedCountries = await fetchCountry(id);
+        setCountry(fetchedCountries);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [id]);
+
+  return { country, isLoading, error };
+};
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,27 @@
 import { Section, Container, CountryInfo, Loader } from 'components';
+import { useFetchCountry } from 'hooks/useFetchCountry';
 
 export const Country = () => {
+  const { country, isLoading, error } = useFetchCountry();
+
+  const { flag, capital, countryName, id, languages, population } =
+    country || {};
   return (
     <Section>
       <Container>
-        <h2>Country</h2>
+        {isLoading && <Loader />}
+        {error && <div>Error!!!</div>}
+
+        {country && (
+          <CountryInfo
+            flag={flag}
+            capital={capital}
+            countryName={countryName}
+            id={id}
+            languages={languages}
+            population={population}
+          />
+        )}
       </Container>
     </Section>
   );
