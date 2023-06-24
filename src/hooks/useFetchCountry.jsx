d512b574f735653f8f95e368dd1f01f56236f67e
Dima Savchuk <118117711+DimaSavchuk@users.noreply.github.com>
2023-06-24 15:20:51 +0300

    commit

--- src/hooks/useFetchCountry.jsx
+++ src/hooks/useFetchCountry.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import { fetchCountry } from 'service/country-service';
 
 export const useFetchCountry = () => {
-  const [country, setCountry] = useState([]);
+  const [country, setCountry] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
